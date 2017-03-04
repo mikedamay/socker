d7b78a57073bce2a19b7d0380066fbc744aba813
mike <mikemay@blueyonder.co.uk>
2017-03-04 13:19:03 +0000

    tweaked minimal

--- minimal.c
+++ minimal.c
@@ -1,6 +1,6 @@
 
 /**
- * create a server and fork a client which sends the server a single message both processes then exit.
+ * create a server and fork a client which sends the server a single message via a tcp socket both processes then exit.
  * where does gcc get sockaddr from - presumably comes with system calls
  * @return 253 is returned from the server socket, number of bytes written is returned by the client
  */
@@ -9,7 +9,7 @@ int main()
     // ip address 127.0.0.1 (16777343) port 11900 (31790)
     struct {short fam; short port; int ip4; int z0; int z1;}
       myaddr = {2, 31790, 16777343, 0, 0 };
-    int sd = socket(2, 1, 0);
+    int sd = socket(2, 1, 0);   // AF_INET, SOCK_STREAM
     bind(sd, (const struct sockaddr *)&myaddr, 16);
     listen(sd, 10);
     int pid = fork();
@@ -22,16 +22,17 @@ int main()
             int numBytes = read(sd2, buffer, sizeof buffer);
             if (numBytes == 0 )
                 break;
+            write(1, numBytes == 16 ? "16 bytes read             " : "wrong read byte count         ", 25);
         }
         return 253;
     }
     else            // client
     {
-        int sd3 = socket(2, 1, 0);
+        int sd3 = socket(2, 1, 0);      // AF_INET, SOCK_STREAM
         int res = connect(sd3, (const struct sockaddr *)&myaddr, 16);
         int numBytes = write(sd3, "now is the time", 16);
         close(sd3);
-        write(1, numBytes == 16 ? "16 bytes written" : "wrong byte count");
+        write(1, numBytes == 16 ? "16 bytes written          " : "wrong write byte count        ", 25);
         return numBytes;
     }
 }
