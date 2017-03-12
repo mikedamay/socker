f3e12a1775d8778ea9efb86156104ea57ae55233
mike <mikemay@blueyonder.co.uk>
2017-03-12 08:57:27 +0000

    added multi-processing - seems to work with edit.php except that mysql install is broken

--- multi_proxy.cpp
+++ multi_proxy.cpp
@@ -53,27 +53,6 @@ bool smulti_proxy(int argc, char ** argv)
         printError(NULL);
         return false;
     }
-    SOCKET sdProxied;
-    struct sockaddr_in sinProxied;
-    if (!populateSockAddr(proxiedHost, proxiedPort, &sinProxied))
-    {
-        return false;
-    }
-    printf("about to create proxied socket\n");
-    if ((sdProxied = socket(AF_INET, SOCK_STREAM, 0)) == SOCKET_FAILED)
-    {
-        char errbuff[SCRATCH_BUFFER_SIZE];
-        sprintf(errbuff,"socker:multi_proxy() failed to connect to proxied socket %s %d\n", proxiedHost, proxiedPort);
-        printError(errbuff);
-        return false;
-    }
-    printf("about to connect to proxied socket\n");
-    if((connect(sdProxied,(struct sockaddr *)&sinProxied,sizeof(sinProxied))) == CONNECT_FAILED)
-    {
-        printError(NULL);
-        return false;
-    }
-
     // listen for connections and echo messages back to the peer
     SESSION_HANDLER_HANDLE shh = createSessionHandler();
     if (shh == SESSION_HANDLER_HANDLE_ERROR)
@@ -81,37 +60,38 @@ bool smulti_proxy(int argc, char ** argv)
         perror("failed to create session handler");
         return false;
     }
-    while (true)
+    printf("socket:proxy()  about to call listen()\n");
+    SOCKET sdProxied;
+    if (listen(sdProxyServer, 10) == SOCKET_FAILED)
     {
-        printf("socket:proxy()  about to call listen()\n");
-        if (listen(sdProxyServer, 10) == SOCKET_FAILED)
+        printError("listen failed:");
+        return false;
+    }
+    else
+    {
+        SOCKET sdClient;
+        char buffer[1024];
+        ssize_t numBytes;
+        ONE_TIME_BUFFER_HANDLE fromClientBuffer;
+        ONE_TIME_BUFFER_HANDLE  toClientBuffer;
+        fd_set rfds;
+        fd_set wfds;
+        fd_set exceptfds;
+        do
         {
-            printError("listen failed:");
-            return false;
-        }
-        else
-        {
-            SOCKET sdClient;
-            printf("socker:multi_proxy() about to call accept()\n");
-            if ((sdClient = accept(sdProxyServer, NULL, NULL)) == SOCKET_FAILED)
+            FD_ZERO(&rfds);
+            FD_ZERO(&wfds);
+            FD_ZERO(&exceptfds);
+            FD_SET(sdProxyServer, &rfds);
+            FD_SET(sdProxyServer, &exceptfds);
+            SESSION_ITER si = getSessionIter(shh);
+            if (si == SESSION_ITER_ERROR)
             {
-                printError("accept failed:");
-                return false;
+                printError("failed to create session iter");
+                break;
             }
-            char buffer[1024];
-            printf("socker:multi_proxy() about to call read()\n");
-            ssize_t numBytes;
-            ONE_TIME_BUFFER_HANDLE fromClientBuffer;
-            ONE_TIME_BUFFER_HANDLE  toClientBuffer;
-            do
+            while (getNextSession(si, &sdClient, &sdProxied, &fromClientBuffer, &toClientBuffer))
             {
-                fd_set rfds;
-                fd_set wfds;
-                fd_set exceptfds;
-                FD_ZERO(&rfds);
-                FD_ZERO(&wfds);
-                FD_ZERO(&exceptfds);
-/*
                 FD_SET(sdProxied, &rfds);
                 FD_SET(sdProxied, &exceptfds);
                 FD_SET(sdClient, &rfds);
@@ -124,125 +104,150 @@ bool smulti_proxy(int argc, char ** argv)
                 {
                     FD_SET(sdClient, &wfds);
                 }
-
-*/
-                FD_SET(sdProxyServer, &rfds);
-                FD_SET(sdProxyServer, &exceptfds);
-                SESSION_ITER si = getSessionIter(shh);
-                if (si == SESSION_ITER_ERROR)
+            }
+            si = SESSION_ITER_ERROR;
+            int fdCap = max(sdProxyServer, getMaxSD(shh)) + 1;
+            select(fdCap, &rfds, &wfds, &exceptfds, NULL);
+            if (FD_ISSET(sdProxyServer, &exceptfds))
+            {
+                printf("socker:multi_proxy() exception in select - proxy server socket\n");
+            }
+            if (FD_ISSET(sdProxyServer, &rfds))
+            {       // new connection from a client
+                printf("socker:multi_proxy() about to call accept()\n");
+                if ((sdClient = accept(sdProxyServer, NULL, NULL)) == SOCKET_FAILED)
                 {
-                    perror("failed to create session iter");
+                    printError("accept failed:");
                     break;
                 }
-                while (getNextSession(si, &sdClient, &sdProxied, &fromClientBuffer, &toClientBuffer))
+                printf("socker:multi_proxy() accepted client %d\n", sdClient);
+                struct sockaddr_in sinProxied;
+                if (!populateSockAddr(proxiedHost, proxiedPort, &sinProxied))
                 {
-                    FD_SET(sdProxied, &rfds);
-                    FD_SET(sdProxied, &exceptfds);
-                    FD_SET(sdClient, &rfds);
-                    FD_SET(sdClient, &exceptfds);
-                    if (availableBytesInOTB(fromClientBuffer) > 0)
-                    {
-                        FD_SET(sdProxied, &wfds);
-                    }
-                    if (availableBytesInOTB(toClientBuffer) > 0)
-                    {
-                        FD_SET(sdClient, &wfds);
-                    }
+                    return false;
                 }
-                int fdCap = max(sdProxyServer, getMaxSD(shh)) + 1;
-                select(fdCap, &rfds, &wfds, &exceptfds, NULL);
-                if (FD_ISSET(sdProxyServer, &exceptfds))
+                printf("socker:multi_proxy() about to create proxied socket\n");
+                if ((sdProxied = socket(AF_INET, SOCK_STREAM, 0)) == SOCKET_FAILED)
                 {
-                    printf("socker:multi_proxy() exception in select - proxy server socket\n");
-                }
-                si = getSessionIter(shh);
-                if (si == SESSION_ITER_ERROR)
-                {
-                    perror("failed to create session iter");
+                    char errbuff[SCRATCH_BUFFER_SIZE];
+                    sprintf(errbuff,"socker:multi_proxy() failed to connect to proxied socket %s %d\n", proxiedHost, proxiedPort);
+                    printError(errbuff);
                     break;
                 }
-                ONE_TIME_BUFFER_HANDLE dummy;
-                while (getNextSession(si, &sdClient, &sdProxied, &dummy, &dummy))
+                printf("socker:multi_proxy() about to connect to proxied socket %d\n", sdProxied);
+                if((connect(sdProxied,(struct sockaddr *)&sinProxied,sizeof(sinProxied))) == CONNECT_FAILED)
                 {
-                    fromClientBuffer = createOneTimeBuffer((char *)"From Client");
-                    toClientBuffer = createOneTimeBuffer((char * )"To Client");
-                    if (FD_ISSET(sdProxied, &exceptfds) || FD_ISSET(sdClient, &exceptfds))
+                    printError(NULL);
+                    break;
+                }
+                char scratch[1024];
+                sprintf(scratch, "From Client %d", sdClient);
+                fromClientBuffer = createOneTimeBuffer(scratch);
+                sprintf(scratch, "To Client %d", sdProxied);
+                toClientBuffer = createOneTimeBuffer(scratch);
+                if (fromClientBuffer == NULL || toClientBuffer == NULL)
+                {
+                    printf("%s", "out of memory adding client");
+                    return false;
+                }
+                if (!addSession(shh, sdClient, sdProxied, fromClientBuffer, toClientBuffer))
+                {
+                    printf("%s", "out of memory creating session");
+                    return false;
+                }
+            }
+            si = getSessionIter(shh);
+            if (si == SESSION_ITER_ERROR)
+            {
+                perror("failed to create session iter");
+                break;
+            }
+            while (getNextSession(si, &sdClient, &sdProxied, &fromClientBuffer, &toClientBuffer))
+            {
+                if (FD_ISSET(sdProxied, &exceptfds) || FD_ISSET(sdClient, &exceptfds))
+                {
+                    printf("socker:multi_proxy() exception in select\n");
+                }
+                if (FD_ISSET(sdClient, &rfds))
+                {
+                    memset(buffer, '\0', sizeof(buffer));
+                    numBytes = recv(sdClient, buffer, sizeof(buffer), 0);
+                    if (numBytes == -1)
                     {
-                        printf("socker:multi_proxy() exception in select\n");
+                        printError("socker:multi_proxy() read failed:");
+                        break;
                     }
-                    if (FD_ISSET(sdClient, &rfds))
+                    else if (numBytes > 0)
                     {
-                        memset(buffer, '\0', sizeof(buffer));
-                        numBytes = recv(sdClient, buffer, sizeof(buffer), 0);
-                        if (numBytes == -1)
-                        {
-                            printError("socker:multi_proxy() read failed:");
-                            break;
-                        }
-                        else if (numBytes > 0)
-                        {
-                            printf("socker:multi_proxy() about to print buffer from client\n");
-                            printf("%s", buffer);
-                            printf("\n");
-                            writeOTB(fromClientBuffer, buffer, numBytes);
-                        }
-                        else
-                        {
-                            printf("socker:multi_proxy() read of zero bytes performed\n");
-                            printError("socker:multi_proxy() zero bytes read error from client");
-                            break;
-                        }
+                        printf("socker:multi_proxy() about to print buffer from client %d\n", sdClient);
+                        //printf("%s", buffer);
+                        //printf("\n");
+                        writeOTB(fromClientBuffer, buffer, numBytes);
                     }
-                    if (FD_ISSET(sdProxied, &rfds))
+                    else
                     {
-                        memset(buffer, '\0', sizeof(buffer));
-                        numBytes = recv(sdProxied, buffer, sizeof(buffer), 0);
-                        if (numBytes == -1)
-                        {
-                            printError("socker:multi_proxy() sdProxied read failed:");
-                            break;
-                        }
-                        else if (numBytes > 0)
-                        {
-                            printf("socker:multi_proxy() about to print buffer from proxied\n");
-                            printf("%s", buffer);
-                            printf("\n");
-                            writeOTB(toClientBuffer, buffer, numBytes);
-                        }
-                        else
-                        {
-                            printf("socker:multi_proxy() proxied read of zero bytes performed\n");
-                            printError("socker:multi_proxy() zero bytes read error from proxied");
-                            break;
-                        }
-                    }
-                    if (FD_ISSET(sdProxied, &wfds))
-                    {
-                        ssize_t numBytesToWrite;
-                        char * writeBuffer;
-                        if (getAndLockOTBForRead(fromClientBuffer, &writeBuffer, &numBytesToWrite))
-                        {
-                            printf("socker:multi_proxy() about to send to proxied\n");
-                            ssize_t numBytesWritten = send(sdProxied, writeBuffer, (size_t)numBytesToWrite, 0);
-                            unlockOTB(fromClientBuffer, numBytesWritten);
-                        }
-                    }
-                    if (FD_ISSET(sdClient, &wfds))
-                    {
-                        ssize_t numBytesToWrite;
-                        char * writeBuffer;
-                        if (getAndLockOTBForRead(toClientBuffer, &writeBuffer, &numBytesToWrite))
-                        {
-                            printf("socker:multi_proxy() about to send to client");
-                            ssize_t numBytesWritten = send(sdClient, writeBuffer, (size_t)numBytesToWrite, 0);
-                            unlockOTB(toClientBuffer, numBytesWritten);
-                        }
+                        printf("socker:multi_proxy() read of zero bytes performed from %d\n", sdClient);
+                        printError("socker:multi_proxy() zero bytes read error from client");
+                        close(sdClient);
+                        close(sdProxied);
+                        destroyOneTimeBuffer(fromClientBuffer);
+                        destroyOneTimeBuffer(toClientBuffer);
+                        removeSession(shh, sdClient);
+                        break;
                     }
                 }
-
-            } while (true);   // while reading a message
-
-            close(sdClient);
-        }   // while listening
+                if (FD_ISSET(sdProxied, &rfds))
+                {
+                    memset(buffer, '\0', sizeof(buffer));
+                    numBytes = recv(sdProxied, buffer, sizeof(buffer), 0);
+                    if (numBytes == -1)
+                    {
+                        printError("socker:multi_proxy() sdProxied read failed:");
+                        break;
+                    }
+                    else if (numBytes > 0)
+                    {
+                        printf("socker:multi_proxy() about to print buffer from proxied %d\n", sdProxied);
+                        //printf("%s", buffer);
+                        //printf("\n");
+                        writeOTB(toClientBuffer, buffer, numBytes);
+                    }
+                    else
+                    {
+                        printf("socker:multi_proxy() proxied read of zero bytes performed from %d\n", sdProxied);
+                        printError("socker:multi_proxy() zero bytes read error from proxied");
+                        close(sdClient);
+                        close(sdProxied);
+                        destroyOneTimeBuffer(fromClientBuffer);
+                        destroyOneTimeBuffer(toClientBuffer);
+                        removeSession(shh, sdClient);
+                        break;
+                    }
+                }
+                if (FD_ISSET(sdProxied, &wfds))
+                {
+                    ssize_t numBytesToWrite;
+                    char * writeBuffer;
+                    if (getAndLockOTBForRead(fromClientBuffer, &writeBuffer, &numBytesToWrite))
+                    {
+                        printf("socker:multi_proxy() about to send to proxied %d -> %d\n", sdClient, sdProxied);
+                        ssize_t numBytesWritten = send(sdProxied, writeBuffer, (size_t)numBytesToWrite, 0);
+                        unlockOTB(fromClientBuffer, numBytesWritten);
+                    }
+                }
+                if (FD_ISSET(sdClient, &wfds))
+                {
+                    ssize_t numBytesToWrite;
+                    char * writeBuffer;
+                    if (getAndLockOTBForRead(toClientBuffer, &writeBuffer, &numBytesToWrite))
+                    {
+                        printf("socker:multi_proxy() about to send to client %d -> %d\n", sdProxied, sdClient);
+                        ssize_t numBytesWritten = send(sdClient, writeBuffer, (size_t)numBytesToWrite, 0);
+                        unlockOTB(toClientBuffer, numBytesWritten);
+                    }
+                }
+            }       // getNextSession()
+            si = SESSION_ITER_ERROR;
+        } while (true);   // while reading a message
     }
 }
